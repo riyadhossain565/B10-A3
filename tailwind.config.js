580205c8d88ac6fdb1d7ca19f5b5f3684850ce24
Riyad Hossain <hossainriyad565@gmail.com>
2024-08-26 19:27:33 +0600

    added new section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "form-img": "url('/assets/bg.jpg')",
+      },
+    },
   },
   plugins: [],
 };
